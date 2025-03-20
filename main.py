fcfd1446cbbba49b6005d5d0f1c43be95bfb224f
npascal971 <68188112+npascal971@users.noreply.github.com>
2025-03-20 11:41:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ symbols = [
 ]
 timeframe = '1h'
 ltf_timeframe = '5m'
-asian_session_start = 14
+asian_session_start = 22
 asian_session_end = 6
 risk_per_trade = 0.01
 max_simultaneous_trades = 1
