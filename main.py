9459a597111eb20d89557d315fbc5da09887962f
npascal971 <68188112+npascal971@users.noreply.github.com>
2025-03-22 15:02:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -234,13 +234,13 @@ class AsianSessionTrader:
                 logging.info(f"🔁 Trailing SL mis à jour pour {symbol} : {trade['sl']:.2f} → {new_sl:.2f}")
                 trade["sl"] = new_sl
 
-        # Break-even
+            #Break-even
             if price >= trade["entry"] * (1 + self.break_even_trigger / 100) and trade["sl"] < trade["entry"]:
                 logging.info(f"🔐 Break-even activé pour {symbol} → SL remonté à l'entrée : {trade['entry']:.2f}")
                 trade["sl"] = trade["entry"]
 
-    except Exception as e:
-        logging.error(f"Erreur SL/TP dynamique : {e}")
+            except Exception as e:
+                logging.error(f"Erreur SL/TP dynamique : {e}")
 
 
     def monitor_trades(self):
